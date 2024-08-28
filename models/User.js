7674d5553ab8efa4fee8b263ccd5fe16206e9276
James Kayes <31102962+j-kayes@users.noreply.github.com>
2024-08-28 20:10:20 +0100

    Updates to use bcrypt instead of the js version.

--- models/User.js
+++ models/User.js
@@ -1,29 +1,23 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
 
-UserSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
-UserSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
+// Method to compare passwords
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model('User', UserSchema);
-
+const User = mongoose.model('User', userSchema);
 module.exports = User;
